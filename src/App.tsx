eced7dab275d52e6e05bdb4ef933906eed689ad2
doedor <doedor311@gmail.com>
2026-05-07 10:39:50 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,65 @@
  */
 
 import { useState, useEffect } from 'react';
-import { faces, Face } from './data/faces';
+export type Gender = 'boy' | 'girl';
+
+export interface Face {
+  id: number;
+  name: string;
+  gender: Gender;
+  imageUrl: string;
+}
+
+export const faces: Face[] = [
+  { id: 1, name: "Micah Abalahin", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7643903_120.jpg?resize=200,200" },
+  { id: 2, name: "Erika Abegg", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7212241_93.jpg?resize=200,200" },
+  { id: 3, name: "Ingrid Abegg", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7193710_726.jpg?resize=200,200" },
+  { id: 4, name: "Elie Christopher Abogo", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7280449_997.jpg?resize=200,200" },
+  { id: 5, name: "Francia Ludmila Abogo", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7637634_611.jpg?resize=200,200" },
+  { id: 6, name: "Lily Ackerman", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7634707_649.jpg?resize=200,200" },
+  { id: 7, name: "Sebastian Aguila", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8301231_227.jpg?resize=200,200" },
+  { id: 8, name: "Elena Aldana", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7934427_276.jpg?resize=200,200" },
+  { id: 9, name: "Blake Alderton", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8274358_938.jpg?resize=200,200" },
+  { id: 10, name: "Pierce Aldridge", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7666745_594.jpg?resize=200,200" },
+  { id: 11, name: "Bethel Alemu", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7921069_5.jpg?resize=200,200" },
+  { id: 12, name: "Beza Alemu", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7150693_574.jpg?resize=200,200" },
+  { id: 13, name: "Christian Jack Alfaro", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8354508_763.jpg?resize=200,200" },
+  { id: 14, name: "Keira Alfaro", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7239786_48.jpg?resize=200,200" },
+  { id: 15, name: "Erin Allen", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7561682_184.jpg?resize=200,200" },
+  { id: 16, name: "Lucy Allen", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7156352_587.jpg?resize=200,200" },
+  { id: 17, name: "Maya Faye Al-Mtwali", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7616467_913.jpg?resize=200,200" },
+  { id: 18, name: "Alyssa Amaya", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8274910_482.jpg?resize=200,200" },
+  { id: 19, name: "Sebastian Amaya", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7638804_715.jpg?resize=200,200" },
+  { id: 20, name: "Henry Amaya-Perez", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7921298_273.jpg?resize=200,200" },
+  { id: 21, name: "Derek Amberg", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7161165_682.jpg?resize=200,200" },
+  { id: 22, name: "Elijah Amond", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7572992_436.jpg?resize=200,200" },
+  { id: 23, name: "Madison Andrade Diaz", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8278227_953.jpg?resize=200,200" },
+  { id: 24, name: "Tyler Anglin", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7923730_580.jpg?resize=200,200" },
+  { id: 25, name: "Andrew Antonio", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7173255_686.jpg?resize=200,200" },
+  { id: 26, name: "Nicholas Araujo", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7335655_636.jpg?resize=200,200" },
+  { id: 27, name: "Azael Araya", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7640611_785.jpg?resize=200,200" },
+  { id: 28, name: "Avalyn Arciaga", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8274654_684.jpg?resize=200,200" },
+  { id: 29, name: "Victoria Arias", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7590870_99.jpg?resize=200,200" },
+  { id: 30, name: "Kelly Arras", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8278400_828.jpg?resize=200,200" },
+  { id: 31, name: "Molly Arras", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7172958_954.jpg?resize=200,200" },
+  { id: 32, name: "Grace Artz", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7962454_791.jpg?resize=200,200" },
+  { id: 33, name: "Sophia Ary", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7269196_562.jpg?resize=200,200" },
+  { id: 34, name: "Lucas Aschenaki", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7973376_207.jpg?resize=200,200" },
+  { id: 35, name: "Anthony Asmar", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8282263_375.jpg?resize=200,200" },
+  { id: 36, name: "Raiden Atienza-Rosser", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7649994_291.jpg?resize=200,200" },
+  { id: 37, name: "Dominic Atwater", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7989224_713.jpg?resize=200,200" },
+  { id: 38, name: "Emma Bahdi", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7255217_225.jpg?resize=200,200" },
+  { id: 39, name: "Jane Bahdi", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7969601_269.jpg?resize=200,200" },
+  { id: 40, name: "Jackson Bahr", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7850291_716.jpg?resize=200,200" },
+  { id: 41, name: "Maddox Baig", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8313736_290.jpg?resize=200,200" },
+  { id: 42, name: "Edward Baker", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7930780_73.jpg?resize=200,200" },
+  { id: 43, name: "Braeden Bakos", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7239945_378.jpg?resize=200,200" },
+  { id: 44, name: "Antonella Balderrama", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7959784_300.jpg?resize=200,200" },
+  { id: 45, name: "Benjamin Balducci", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7545156_789.jpg?resize=200,200" },
+  { id: 46, name: "Elliot Baltazar Galdos", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8332371_528.jpg?resize=200,200" },
+  { id: 47, name: "Robert Baney", gender: "boy", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_7217328_752.jpg?resize=200,200" },
+  { id: 48, name: "Meredith Barker", gender: "girl", imageUrl: "https://bbk12e1-cdn.myschoolcdn.com/ftpimages/847/user/large_user_8297536_572.jpg?resize=200,200" }
+];
 
 type Filter = 'boys' | 'girls' | 'both';
 
